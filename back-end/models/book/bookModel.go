43e1a0197fc2d51879b195edac6f4a10b0556981
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-05-02 15:36:37 -0300

    feat: finish bookModel

--- back-end/models/book/bookModel.go
+++ back-end/models/book/bookModel.go
@@ -7,3 +7,29 @@ type UseCase interface {
 	Update(b *Book) error
 	Delete(Id int64) error
 }
+
+type Service struct{}
+
+func NewService() *Service {
+	return &Service{}
+}
+
+func (s *Service) GetAll() ([]*Book, error) {
+	return nil, nil
+}
+
+func (s *Service) GetById() (*Book, error) {
+	return nil, nil
+}
+
+func (s *Service) Create() error {
+	return nil
+}
+
+func (s *Service) Update() error {
+	return nil
+}
+
+func (s *Service) Delete() error {
+	return nil
+}
