06e5a627da5afd6b8a698f5561c237a5f422e2e1
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-05-03 16:35:21 -0300

    feat: changing from pure sql to GORM

--- back-end/database/database.go
+++ back-end/database/database.go
@@ -1,45 +1,62 @@
 package database
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
 	"os"
 	"time"
 
-	_ "github.com/go-sql-driver/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 // Atribuímos o database para a variável db, tornando seu uso mais simples.
-var db sql.DB
+var db *gorm.DB
 
 func InitDB() {
 	// Criamos a string de conexão usando os métodos fmt.Sprintf + os.Getenv
 	// para sigilo de informações delicadas.
 	var connectionString string = fmt.Sprintf(
-		"%s:%s@tcp(%s)/%s",
+		"%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_NAME"),
 	)
 
-	db, err := sql.Open("mysql", connectionString)
+	database, err := gorm.Open(mysql.Open(connectionString), &gorm.Config{})
 
 	// Caso o erro não seja nulo, tratamos ele
 	if err != nil {
-		log.Fatal("something went wrong: ", err)
+		fmt.Println("something went wrong, couldn't connect with mysql database")
+		log.Fatal("error: ", err)
 	}
 
 	fmt.Println("db running")
 
-	// https://github.com/go-sql-driver/mysql#important-settings
-	db.SetConnMaxLifetime(time.Hour)
-	db.SetMaxOpenConns(20)
-	db.SetMaxIdleConns(20)
+	// https://gorm.io/docs/index.html
+	db = database
+	config, _ := db.DB()
+	config.SetMaxIdleConns(10)
+	config.SetMaxOpenConns(100)
+	config.SetConnMaxLifetime(time.Hour)
+}
+
+func CloseConnection() error {
+	config, err := db.DB()
+	if err != nil {
+		return err
+	}
+
+	err = config.Close()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // Função para exportar nosso database e realizar querys
-func GetDB() *sql.DB {
-	return &db
+func GetDB() *gorm.DB {
+	return db
 }
