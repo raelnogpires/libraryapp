b9fd50ba2ebc8eeb3c9603fc95a4fa094fd54016
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-05-03 17:23:33 -0300

    feat: finish server.go

--- back-end/server/server.go
+++ back-end/server/server.go
@@ -1,9 +1,11 @@
 package server
 
 import (
+	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/raelnogpires/libraryapp/back-end/server/routes"
 )
 
 type Server struct {
@@ -16,4 +18,11 @@ func NewServer() Server {
 		port:   os.Getenv("SERVER_PORT"),
 		server: gin.Default(),
 	}
+}
+
+func (s *Server) Run() {
+	router := routes.ConfigRoutes(s.server)
+
+	log.Printf("server running at port: %s", s.port)
+	log.Fatal(router.Run(":" + s.port))
 }
