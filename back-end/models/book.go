5d2f3f1e71514d51f9a1671c07bb63ee60bb2b5a
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-05-03 21:53:13 -0300

    new branch to fix foreignKey issue

--- back-end/models/book.go
+++ back-end/models/book.go
@@ -1,12 +1,13 @@
 package models
 
 type Book struct {
-	ID          int64    `json:"id" gorm:"primaryKey"`
-	Name        string   `json:"name"`
-	Description string   `json:"description"`
-	CategoryId  int64    `json:"category_id"`
-	AuthorId    int64    `json:"author_id"`
-	Category    Category `gorm:"foreignKey:ID"`
-	Author      Author   `gorm:"foreignKey:ID"`
-	ImgUrl      string   `json:"img_url"`
+	ID          int64  `json:"id" gorm:"primaryKey"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	CategoryId  int64  `json:"category_id"`
+	AuthorId    int64  `json:"author_id"`
+	// will be fixing this
+	Category Category `gorm:"foreignKey:ID"`
+	Author   Author   `gorm:"foreignKey:ID"`
+	ImgUrl   string   `json:"img_url"`
 }
