a41ce345239779f53b6683c0fe03612a3bafa3ac
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-05-09 19:01:19 -0300

    feat: create category service layer

--- back-end/services/categoryService.go
+++ back-end/services/categoryService.go
@@ -1,1 +1,75 @@
 package services
+
+import (
+	"github.com/raelnogpires/libraryapp/back-end/database"
+	"github.com/raelnogpires/libraryapp/back-end/models"
+)
+
+func GetAllCategories() ([]*models.Category, error) {
+	db := database.GetDB()
+	var cat []*models.Category
+
+	err := db.Find(&cat).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return cat, nil
+}
+
+func GetCategoryById(ID int) (*models.Category, error) {
+	db := database.GetDB()
+	var cat *models.Category
+
+	err := db.Find(&cat, ID).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return cat, nil
+}
+
+func CreateCategory(cat *models.Category) error {
+	db := database.GetDB()
+
+	err := db.Create(&cat).Error
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func EditCategory(cat *models.Category) error {
+	db := database.GetDB()
+	var check models.Category
+
+	err := db.First(&check, cat.ID).Error
+	if err != nil {
+		return err
+	}
+
+	err = db.Save(&cat).Error
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func DeleteCategory(ID int) error {
+	db := database.GetDB()
+	var cat *models.Category
+
+	err := db.First(&cat, ID).Error
+	if err != nil {
+		return err
+	}
+
+	err = db.Delete(&models.Category{}, ID).Error
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
