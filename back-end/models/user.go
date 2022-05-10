dfda52e8f90e3cd32c28b3ff363030be293588d8
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-05-10 10:59:31 -0300

    fix: removes unnecessary stryct

--- back-end/models/user.go
+++ back-end/models/user.go
@@ -6,8 +6,3 @@ type User struct {
 	Email    string `json:"email" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
-
-// type UserLogin struct {
-// 	Email    string `json:"email" binding:"required"`
-// 	Password string `json:"password" binding:"required"`
-// }
