32cc17b0145c374505bc4871287574a1d49a4b7e
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-05-11 11:09:27 -0300

    revert: removes unnecessary code

--- back-end/database/migrations/migrations.go
+++ back-end/database/migrations/migrations.go
@@ -10,14 +10,4 @@ func RunMigrations(db *gorm.DB) {
 	db.AutoMigrate(models.Category{})
 	db.AutoMigrate(models.User{})
 	db.AutoMigrate(models.Book{})
-
-	// https://www.w3schools.com/sql/sql_foreignkey.asp
-	// apparently GORM doesn't create FK's in AutoMigrate even if its tagged in the Model
-	// so this is the way to go
-	db.Exec(
-		"ALTER TABLE books ADD FOREIGN KEY (category_id) REFERENCES categories(id)",
-	)
-	db.Exec(
-		"ALTER TABLE books ADD FOREIGN KEY (author_id) REFERENCES authors(id)",
-	)
 }
